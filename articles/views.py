aa7ff71e579210b398aef75e1fa5b23e415cf0f1
kka712 <rlska213@naver.com>
2025-03-20 15:13:06 +0900

    Update기능 구현

--- articles/views.py
+++ articles/views.py
@@ -43,3 +43,28 @@ def delete(request, id):
     article.delete()
 
     return redirect('articles:index')
+
+
+def edit(request, id):
+    article = Article.objects.get(id=id)
+
+    context = {
+        'article': article
+    }
+
+    return render(request, 'edit.html', context)
+    
+
+def update(request, id):
+    # 기존정보
+    article = Article.objects.get(id=id)
+
+    # 새로운정보
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+
+    article.title = title
+    article.content = content
+    article.save()
+
+    return redirect('articles:detail', id=article.id)
